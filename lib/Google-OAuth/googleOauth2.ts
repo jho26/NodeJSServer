9139968b01d54f70d0dfce599cef9e17ac3b93ec
Jonathan Ho <uschohk@hotmail.com>
2019-06-09 16:49:03 -0700

    fix oauth credentials

--- lib/Google-OAuth/googleOauth2.ts
+++ lib/Google-OAuth/googleOauth2.ts
@@ -1,7 +1,7 @@
 class googleOauth2 {
     // NOTE : this Oauth is linked to a different url. Nobody can test this except me for now!!
-    static id: string = '574577622242-c8vtfujpg5ubbl5nppebba6ccra5cjle.apps.googleusercontent.com';
-    static secret:string = 'higsuTCtXgOQqAnNvY-Revfx';
+    static id: string = '553594013008-q8ogek4n1nqeskmr3f2uec691o0uifif.apps.googleusercontent.com';
+    static secret:string = '-5aBsSqoOZL-HQZYBUNSCDsU';
     
     }
     export default googleOauth2;
